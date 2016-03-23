b09dac45c16701bdf0b893458384652af63a62a9
mykolahnid <g.mykola@gmail.com>
2016-03-23 17:12:05 +0200

    initial commit

---
+++ Frevent.Model/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frevent.Model.Models
+{
+    public class Event
+    {
+        public int EventID { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
