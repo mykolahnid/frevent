0327f687326b20da0265065b3a380010038fc937
mykolahnid <g.mykola@gmail.com>
2016-03-25 21:31:46 +0200

    Service project

---
+++ Frevent.Service/EventService.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Frevent.Data.Infrastructure;
+using Frevent.Data.Repositories;
+using Frevent.Model.Models;
+
+namespace Frevent.Service
+{
+    public interface IEventService
+    {
+        IEnumerable<Event> GetEvents();
+        Event GetEvent(int id);
+        void CreateEvent(Event gadget);
+        void SaveEvent();
+    }
+
+    public class EventService : IEventService
+    {
+        private readonly IEventRepository eventRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public EventService(IEventRepository eventRepository, IUnitOfWork unitOfWork)
+        {
+            this.eventRepository = eventRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        #region IEventService members
+
+        public IEnumerable<Event> GetEvents()
+        {
+            var events = eventRepository.GetAll();
+            return events;
+        }
+
+        public Event GetEvent(int id)
+        {
+            var anEvent = eventRepository.GetById(id);
+            return anEvent;
+        }
+
+        public void CreateEvent(Event anEvent)
+        {
+            eventRepository.Add(anEvent);
+        }
+
+        public void SaveEvent()
+        {
+            unitOfWork.Commit();
+        }
+
+        #endregion
+    }
+}
---
+++ Frevent.Service/PlaceService.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Frevent.Data.Infrastructure;
+using Frevent.Data.Repositories;
+using Frevent.Model.Models;
+
+namespace Frevent.Service
+{
+    public interface IPlaceService
+    {
+        IEnumerable<Place> GetPlaces();
+        Place GetPlace(int id);
+        void CreatePlace(Place gadget);
+        void SavePlace();
+    }
+
+    public class PlaceService : IPlaceService
+    {
+        private readonly IPlaceRepository placeRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public PlaceService(IPlaceRepository placeRepository, IUnitOfWork unitOfWork)
+        {
+            this.placeRepository = placeRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        #region IPlaceService members
+
+        public IEnumerable<Place> GetPlaces()
+        {
+            var places = placeRepository.GetAll();
+            return places;
+        }
+
+        public Place GetPlace(int id)
+        {
+            var place = placeRepository.GetById(id);
+            return place;
+        }
+
+        public void CreatePlace(Place place)
+        {
+            placeRepository.Add(place);
+        }
+
+        public void SavePlace()
+        {
+            unitOfWork.Commit();
+        }
+
+        #endregion
+    }
+}
